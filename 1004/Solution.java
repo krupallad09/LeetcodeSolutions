718fde44a9f0e9f59cf3efc4fc9e019246b2d13a
krupallad09 <krupal.lad786@gmail.com>
2025-04-11 15:23:18 +0530

    Add solution for longest consecutive ones problem

---
+++ 1004/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int left = 0, right = 0, zeroes = 0, maxLen = 0;
+        int len = 0;
+        while(right < nums.length) {
+            if(nums[right] == 0) zeroes++;
+            if(zeroes > k) {
+                if(nums[left] == 0) zeroes --;
+                left++;
+            }
+
+            if(zeroes <= k) {
+                len = right - left + 1;
+                maxLen = Math.max(maxLen, len);
+            }
+            right++;
+        }
+        return maxLen;
+    }
+}
